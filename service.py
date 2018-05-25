03b92127b11b2e1e5feb09585e9b1975af2112c9
Dan Kolbman <dankolbman@gmail.com>
2018-05-24 22:49:13 -0400

    :sparkles: Send request response to logger

--- service.py
+++ service.py
@@ -1,8 +1,27 @@
+import time
+import logging
 from botocore.vendored import requests
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def handler(event, context):
     """
     Entry point to the lambda function
     """
-    requests.get('http://kids-first.io')
+    SERVICE = event['service']
+    ENDPOINT = event['endpoint']
+
+    t0 = time.time()
+    resp = requests.get(SERVICE+ENDPOINT)
+    t1 = time.time()
+
+    code = resp.status_code
+
+    logger.info('{} {} {} {}'.format(SERVICE, ENDPOINT, code, t1-t0))
+
+
+if __name__ == '__main__':
+    handler({'service': 'http://google.com', 'endpoint': '/'}, {})
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,9 +1,20 @@
 import pytest
 import service
+from mock import patch
 
 
 def test_handler():
     """
     Test the service handler
     """
-    service.handler({}, {})
+    r_patch = patch('service.requests')
+    r_mock = r_patch.start()
+
+    event = {
+        'service': 'http://kids-first.github.io',
+        'endpoint': '/'
+    }
+    service.handler(event, {})
+
+    assert r_mock.get.call_count == 1
+    r_mock.get.assert_called_with('http://kids-first.github.io/')
