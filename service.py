a929b2a988e6501f8b0d5bb3b27968a91757f018
Dan Kolbman <dankolbman@gmail.com>
2018-06-05 10:41:58 -0400

    :recycle: Update endpoint list

--- service.py
+++ service.py
@@ -23,27 +23,45 @@ def handler(event, context):
 
     # To stdout/cloudwatch log
     logger.info('{} {} {} {}'.format(SERVICE, ENDPOINT, code, t1-t0))
+    metrics = [
+        {
+            'MetricName': 'ResponseTime'+ENDPOINT.replace('/',''),
+            'Dimensions': [
+                {
+                    'Name': 'endpoint',
+                    'Value': ENDPOINT
+                },
+                {
+                    'Name': 'service',
+                    'Value': SERVICE 
+                }
+            ],
+            'Timestamp': time.time(),
+            'Value': t1-t0,
+            'Unit': 'Seconds'
+        },
+        {
+            'MetricName': 'ErrorCount'+ENDPOINT.replace('/',''),
+            'Dimensions': [
+                {
+                    'Name': 'endpoint',
+                    'Value': ENDPOINT
+                },
+                {
+                    'Name': 'service',
+                    'Value': SERVICE 
+                }
+            ],
+            'Timestamp': time.time(),
+            'Value': 1 if code >= 400 else 0,
+            'Unit': 'Count'
+        }
+    ]
+
     # To cloudwatch metric directly
     client = boto3.client('cloudwatch', region_name='us-east-1')
     response = client.put_metric_data(
 	Namespace='HealthChecks',
-	MetricData=[
-	    {
-		'MetricName': 'ResponseTime'+ENDPOINT.replace('/',''),
-		'Dimensions': [
-		    {
-			'Name': 'endpoint',
-			'Value': ENDPOINT
-		    },
-		    {
-			'Name': 'service',
-			'Value': SERVICE 
-		    }
-		],
-		'Timestamp': time.time(),
-		'Value': t1-t0,
-		'Unit': 'Seconds'
-	    }
-	]
+	MetricData=metrics
     )
     return '{} {} {} {}'.format(SERVICE, ENDPOINT, code, t1-t0)
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,6 +1,6 @@
 import pytest
 import service
-from mock import patch
+from mock import patch, Mock
 from moto import mock_cloudwatch
 
 
@@ -11,6 +11,30 @@ def test_handler():
     """
     r_patch = patch('service.requests')
     r_mock = r_patch.start()
+    mock_resp = Mock()
+    mock_resp.status_code = 200
+    r_mock.get.return_value = mock_resp
+
+    event = {
+        'service': 'http://kids-first.github.io',
+        'endpoint': '/'
+    }
+    service.handler(event, {})
+
+    assert r_mock.get.call_count == 1
+    r_mock.get.assert_called_with('http://kids-first.github.io/')
+
+
+@mock_cloudwatch
+def test_bad_status():
+    """
+    Test bad status code
+    """
+    r_patch = patch('service.requests')
+    r_mock = r_patch.start()
+    mock_resp = Mock()
+    mock_resp.status_code = 200
+    r_mock.get.return_value = mock_resp
 
     event = {
         'service': 'http://kids-first.github.io',
